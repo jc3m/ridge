5a33c786e977940895a3a124113a82d208604aae
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-12-01 16:19:06 -0600

    Strip grpc, add gorilla mux and dotenv

--- server.go
+++ server.go
@@ -1,41 +1,23 @@
 package main
 
 import (
-  "context"
-  "fmt"
   "log"
-  "net"
-  "time"
+  "net/http"
 
-  pb "github.com/jc3m/ridge/generated"
-  "google.golang.org/grpc"
+  "github.com/gorilla/mux"
+  "github.com/joho/godotenv"
 )
 
 // TODO: Read from flag
 var port string = "8000"
 
-type server struct {}
-
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
-  log.Printf("Received: %v", in.GetName())
-  return &pb.HelloResponse{Greeting: "Hello " + in.GetName()}, nil
-}
-
 func main() {
-  lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", port))
-  if err != nil {
-    log.Fatalf("Error listening: %v", err)
-  }
+  // Loads from .env file. Fails silently if .env doesn't exist
+  godotenv.Load()
 
-  s := grpc.NewServer(
-    grpc.ConnectionTimeout(30 * time.Second), // TODO: Make this a global
-  )
-  pb.RegisterHelloServer(s, &server{})
+  r := mux.NewRouter()
 
-  log.Printf("Listening...")
-  err = s.Serve(lis)
-  if err != nil {
-    log.Fatalf("failed to serve: %v", err)
-  }
+  // TODO: Restrict to https
+  http.Handle("/", r)
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
